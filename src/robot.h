0f8b48f4fca9d8651e1a62ca3fa993c14b163742
Anton Eriksson <antoneri@gmail.com>
2014-11-03 21:26:10 +0100

    Made turn_left etc... private methods

--- src/robot.h
+++ src/robot.h
@@ -7,19 +7,19 @@ class Robot {
   private:
     Motor left;
     Motor right;
-    void direction(int l, int r);
-    void speed(unsigned int l, unsigned int r);
     void setup();
+    void speed(unsigned int l, unsigned int r);
+    void direction(int l, int r);
+    void turn_right(unsigned int l, unsigned int r);
+    void turn_left(unsigned int l, unsigned int r);
+    void go_forward(unsigned int l, unsigned int r);
+    void go_backward(unsigned int l, unsigned int r);
   public:
     typedef struct track {
         int direction;
         int speed;
     };
     Robot();
-    void turn_right(unsigned int l, unsigned int r);
-    void turn_left(unsigned int l, unsigned int r);
-    void go_forward(unsigned int l, unsigned int r);
-    void go_backward(unsigned int l, unsigned int r);
     void run(track *tracks);
     void stop();
 };
